b6363e1a644b2c0ef0528287f9f2312ed62a8731
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2026-06-03 20:25:19 +0300

    fix(a11y): regenerate segment_tech.js paraglide output (missed in 337d2f8)

--- src/i18n/paraglide/messages/segment_tech.js
+++ src/i18n/paraglide/messages/segment_tech.js
@@ -6,17 +6,17 @@ import { getLocale, experimentalStaticLocale } from '../runtime.js';
 /** @typedef {{}} Segment_TechInputs */
 
 const uk_segment_tech = /** @type {(inputs: Segment_TechInputs) => LocalizedString} */ () => {
-	return /** @type {LocalizedString} */ (`Технічна інформація`)
+	return /** @type {LocalizedString} */ (`бітрейт`)
 };
 
 const en_segment_tech = /** @type {(inputs: Segment_TechInputs) => LocalizedString} */ () => {
-	return /** @type {LocalizedString} */ (`Technical info`)
+	return /** @type {LocalizedString} */ (`bitrate`)
 };
 
 /**
 * | output |
 * | --- |
-* | "Technical info" |
+* | "bitrate" |
 *
 * @param {Segment_TechInputs} inputs
 * @param {{ locale?: "uk" | "en" }} options
