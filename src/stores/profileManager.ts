5fd3775d40778ecbeebb1394bf251e41faf4ada2
Ruslan Iskov <ruslan.rv.ua@gmail.com>
2026-06-20 10:18:31 +0300

    feat(profiles): $profilesSelection atom

--- src/stores/profileManager.ts
+++ src/stores/profileManager.ts
@@ -2,3 +2,6 @@ import { atom } from "nanostores";
 import type { ProfileMeta } from "../lib/tauri";
 
 export const $profileList = atom<ProfileMeta[]>([]);
+
+/** Multi-select state for the profiles list (milestone D). Keyed by profile name. */
+export const $profilesSelection = atom<Set<string>>(new Set());
